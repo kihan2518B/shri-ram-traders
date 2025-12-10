27d38ed89edfdbe6e5e0f1b06a991472991a8e1f
kihan2518B <kp648027@gmail.com>
2025-12-10 20:32:44 +0530

    solved build error

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,38 +1,36 @@
-"use client"
-import type React from "react"
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import CustomAdminSideBar from "@/components/CustomAdminSidebar"
-import { SessionContextProvider } from '@supabase/auth-helpers-react';
-import { Toaster } from 'react-hot-toast';
-import { supabase } from "@/utils/supabase";
+"use client";
+import type React from "react";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import CustomAdminSideBar from "@/components/CustomAdminSidebar";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { Toaster } from "react-hot-toast";
+import { supabaseClient } from "@/utils/supabase";
 
 export default function DashboardLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode;
 }) {
-    return (
-        <div className="w-full">
-            <SidebarProvider>
-                <SidebarTrigger className="" />
-                <CustomAdminSideBar />
-                <SessionContextProvider supabaseClient={supabase}>
-                    {children}
-                    <Toaster
-                        position="top-right"
-                        reverseOrder={false}
-                        toastOptions={{
-                            style: {
-                                fontFamily: 'inherit',
-                                fontSize: '0.875rem',
-                                fontWeight: '500',
-                            },
-                        }}
-                    />
-                </SessionContextProvider>
-            </SidebarProvider>
-        </div>
-    )
+  return (
+    <div className="w-full">
+      <SidebarProvider>
+        <SidebarTrigger className="" />
+        <CustomAdminSideBar />
+        <SessionContextProvider supabaseClient={supabaseClient}>
+          {children}
+          <Toaster
+            position="top-right"
+            reverseOrder={false}
+            toastOptions={{
+              style: {
+                fontFamily: "inherit",
+                fontSize: "0.875rem",
+                fontWeight: "500",
+              },
+            }}
+          />
+        </SessionContextProvider>
+      </SidebarProvider>
+    </div>
+  );
 }
-
-
